fc3f789b2fc46784cb00bc7c23020e341684d7c9
sxntiagoad <salvarezd3@eafit.edu.co>
2024-08-12 12:07:58 -0500

    Start creating models

--- djangoIA/postings/models.py
+++ djangoIA/postings/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    # id = models.CharField(max_length=)
+    name = models.CharField(max_length=30),
+    lastname = models.CharField(max_length=30),
+    
