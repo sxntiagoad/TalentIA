8e7c7eb0750880afae43d9d9feeb2e047553d266
nricom <nricom@eafit.edu.co>
2024-08-12 15:36:00 -0500

    Update

--- djangoIA/TalentIA/settings.py
+++ djangoIA/TalentIA/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'postings'
+    
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'TalentIA.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'localhost',
+        'PORT': 5432 ,
+        'NAME': 'DataBase_TalentIA', #Definir nombre de la base de datos
+        'USER': 'postgres',
+        'PASSWORD' : '14789632',
     }
 }
 
@@ -121,3 +127,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configuración de archivos multimedia
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- djangoIA/postings/admin.py
+++ djangoIA/postings/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User, Service, Category, Subcategory
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Service)
+admin.site.register(Category)
+admin.site.register(Subcategory)
