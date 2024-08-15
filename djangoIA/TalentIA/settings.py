3148604a04671165740285004a5ee4e70d8d0117
nricom <nricom@eafit.edu.co>
2024-08-14 19:52:42 -0500

    Update Data base to sqlite

--- djangoIA/TalentIA/settings.py
+++ djangoIA/TalentIA/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'TalentIA.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': 'localhost',
-        'PORT': 5432 ,
-        'NAME': 'DataBase_TalentIA', #Definir nombre de la base de datos
-        'USER': 'postgres',
-        'PASSWORD' : '14789632',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
