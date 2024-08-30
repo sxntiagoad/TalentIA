c0716ace386b5557587eafe6a43252e9999b477f
sxntiagoad <salvarezd3@eafit.edu.co>
2024-08-30 13:44:33 -0500

    update

---
+++ react-talentia/src/components/home/CategoriesList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { getAllCategories, getCategoryById, getAllSubCategories, getSubCategoryById, getAllNestedCategories, getNestedCategoryById } from '../../api/Categories.api';
+
+export function CategoriesList() {
+  return (
+    <div>CategoriesList</div>
+  )
+}
+
+export default CategoriesList
