cf326bef96cb693485169824eff936ba8acd3890
sxntiagoad <salvarezd3@eafit.edu.co>
2024-09-02 15:49:00 -0500

    update

--- djangoIA/postings/urls.py
+++ djangoIA/postings/urls.py
@@ -1,7 +1,7 @@
 # urls.py
 from django.urls import path, include
 from rest_framework import routers
-from .views import UserViewSet, CategoryViewSet, SubcategoryViewSet, ServiceViewSet, NestedCategoryViewSet, JobViewSet, CompanyViewSet
+from .views import UserViewSet, CategoryViewSet, SubcategoryViewSet, ServiceViewSet, NestedCategoryViewSet, JobViewSet, CompanyViewSet, search_items
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -12,6 +12,8 @@ router.register(r'services', ServiceViewSet)
 router.register(r'jobs', JobViewSet)
 router.register(r'companies', CompanyViewSet)
 
+
 urlpatterns = [
     path('api/v1/', include(router.urls)),
+    path('api/v1/search/', search_items, name='search_items'),
 ]
--- react-talentia/src/pages/HomePage.jsx
+++ react-talentia/src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import { ItemsList } from "../components/home/ItemsList";
 import { Banner } from "../components/home/Banner"; // Importa el Banner
 import { CategoriesList } from "../components/home/CategoriesList"; // Importa el componente CategoriesList
-import { Navigation } from "../components/general/Navigation"; // Importa el componente Navigation
+import { Navigation } from "../components/general/Navbar"; // Importa el componente Navigation
 
 
 export function HomePage() {
