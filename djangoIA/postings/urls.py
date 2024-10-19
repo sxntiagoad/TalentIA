e5ecd53d2509b27807c0127c3a70bc69fda0beba
Santiago Alvarez <salvarezd3@eafit.edu.co>
2024-10-19 15:38:09 -0500

    refactor posting views

--- djangoIA/postings/urls.py
+++ djangoIA/postings/urls.py
@@ -13,13 +13,6 @@ from .views import (
     jobs_by_subcategory,
     create_job,
     create_service,
-    job_creation_step1,
-    job_creation_step2,
-    job_creation_step3,
-    job_creation_final_review,
-    service_creation_step1,
-    service_creation_step2,
-    service_creation_final_review,
     nestedcategories_by_subcategory
 )
 
@@ -39,13 +32,6 @@ urlpatterns = [
     path('api/v1/subcategories/category/<int:category_id>/', subcategories_by_category, name='subcategories-by-category'),
     path('api/v1/create-job/', create_job, name='create-job'),
     path('api/v1/create-service/', create_service, name='create-service'),
-    path('job/create/step1/', job_creation_step1, name='job_creation_step1'),
-    path('job/create/step2/<int:job_id>/', job_creation_step2, name='job_creation_step2'),
-    path('job/create/step3/<int:job_id>/', job_creation_step3, name='job_creation_step3'),
-    path('job/create/final-review/<int:job_id>/', job_creation_final_review, name='job_creation_final_review'),
-    path('service/create/step1/', service_creation_step1, name='service_creation_step1'),
-    path('service/create/step2/<int:service_id>/', service_creation_step2, name='service_creation_step2'),
-    path('service/create/final-review/<int:service_id>/', service_creation_final_review, name='service_creation_final_review'),
     path('api/v1/nestedcategories/subcategory/<int:subcategory_id>/', 
          nestedcategories_by_subcategory, 
          name='nestedcategories-by-subcategory'),
