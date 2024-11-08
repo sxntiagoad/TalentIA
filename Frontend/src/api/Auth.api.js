b334ff34368e165e9d5560a6d24c0d3d42adccd7
nricom <nricom@eafit.edu.co>
2024-11-08 10:59:18 -0500

    update

--- Frontend/src/api/Auth.api.js
+++ Frontend/src/api/Auth.api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://http://34.69.134.180/accounts/';
+const API_URL = 'http://34.69.134.180/accounts/';
 
 const authAxios = axios.create({
   baseURL: API_URL,
