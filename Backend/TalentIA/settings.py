f6b68ce35df8095f8b9c8d85a5d35716bc73d7f1
nricom <nricom@eafit.edu.co>
2024-11-07 21:53:01 -0500

    fix

--- Backend/TalentIA/settings.py
+++ Backend/TalentIA/settings.py
@@ -143,7 +143,8 @@ MEDIA_URL = '/media/'
 CORS_ALLOWED_ORIGINS = [
     "http://34.121.119.251",
 ]
-CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_ALL_ORIGINS = True  # Solo para desarrollo, configura apropiadamente para producción
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
